e4e9cc2ded2ab02d132b8ad53b72d894220e2be4
yul.bahat <ybahat@gmail.com>
2021-11-29 23:29:06 +0800

    First REST

--- run.py
+++ run.py
@@ -3,10 +3,15 @@ from flask import Flask, jsonify, request
 my_awesome_app = Flask(__name__)
 
 
-@my_awesome_app.route('/')
+@my_awesome_app.route('/', methods=['POST'])
 def hello_world():
     params = request.get_json()
     return jsonify(params)
+
+
+@my_awesome_app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
